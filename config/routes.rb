1442c4d9c68bb8bd02cc7f616bc9c50f7903ab8d
Shawn Kohn <shawnkohn@gmail.com>
2015-10-29 09:08:53 -0400

    Added angular install, first /angular_test working

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  get "angular_test", to: "angular_test#index"
+
+   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
   # Example resource route with options:
